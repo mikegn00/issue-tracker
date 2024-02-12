b00a1b7d35bc349675e0c688731d795bffffd502
mikegn00 <michaelgn00@gmail.com>
2024-02-12 16:59:22 +0000

    Added error message and spinner

---
+++ app/components/Spinner.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    // <!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com --> 
+    <div
+      className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status">
+      <span
+        className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+        >Loading...</span>
+    </div>  
+    )
+}
+
+export default Spinner
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,32 @@
 'use client'
 
-import { Button, TextArea, TextField, Callout } from '@radix-ui/themes';
+import { Button, TextArea, TextField, Callout, Text } from '@radix-ui/themes';
 import React, { useState } from 'react';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit} = useForm<IssueForm>();
+const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
   const [error, setError] = useState('');
+  const [isSubmitted, setSubmitted] = useState(false);
   
   return (
     <div className='max-w-xl'>
@@ -26,21 +36,31 @@ const NewIssuePage = () => {
       <form className=' space-y-3' 
         onSubmit={handleSubmit( async (data) => {
           try {
+            setSubmitted(true);
             await axios.post('/api/issues', data);
             router.push('/issues');          
           } catch (error) {
+            setSubmitted(false);
             setError('An unexpected error occured.');
           }
         })}>
           <TextField.Root>
             <TextField.Input placeholder='Title' {...register('title')} />
           </TextField.Root>
+          
+          <ErrorMessage>
+            {errors.title?.message}
+          </ErrorMessage>
+
           <Controller
             name='description'
             control={control}
             render={({ field }) => <SimpleMDE placeholder='Description' {...field}/>}
-            />
-          <Button>Submit New Issue</Button>
+          />
+          <ErrorMessage >
+            {errors.description?.message}
+          </ErrorMessage>
+          <Button disabled={isSubmitted}>Submit New Issue{isSubmitted && <Spinner/>}</Button>
       </form>
     </div>
   )
