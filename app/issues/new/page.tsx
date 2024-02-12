d8babe2c63ee1c35f88504787dc2dda2024da918
mikegn00 <michaelgn00@gmail.com>
2024-02-12 14:29:54 +0000

    Handled form submission
    Handled form submission for creating new issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,23 +1,28 @@
 'use client'
 
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import { Button, TextArea, TextField } from '@radix-ui/themes';
+import React from 'react';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 interface IssueForm {
   title: string;
   description: string;
 }
 
-
-
 const NewIssuePage = () => {
+  const router = useRouter();
   const { register, control, handleSubmit} = useForm<IssueForm>();
+  
   return (
     <form className='max-w-xl space-y-3' 
-      onSubmit={handleSubmit((data) => console.log(data))}>
+      onSubmit={handleSubmit( async (data) => {
+        await axios.post('/api/issues', data);
+        router.push('/issues');
+      })}>
         <TextField.Root>
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@radix-ui/themes';
 import Link from 'next/link';
 const IssuesPage = () => {
   return (
-    <div><Button><Link href='/issues/new'></Link></Button></div>
+    <div><Button><Link href='/issues/new'>New Issue</Link></Button></div>
   )
 }
 
